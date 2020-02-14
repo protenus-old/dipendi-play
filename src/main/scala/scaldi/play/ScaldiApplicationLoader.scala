32605dd23edfb85cc0102c62a1998d29340ca5dd
April <april@protenus.com>
2020-02-14 13:22:26 -0500

    Update to support 2.13 and Play 2.7.x
    Update project to support Scala 2.11 through 2.13.
    
    Update project to support Play Framework 2.7.x.
    
    Update CI configuration and add automatic CI publishing.
    
    Add type annotations to public members and implicits.

--- src/main/scala/scaldi/play/ScaldiApplicationLoader.scala
+++ src/main/scala/scaldi/play/ScaldiApplicationLoader.scala
@@ -8,13 +8,14 @@ import scaldi._
 class ScaldiApplicationLoader(val builder: ScaldiApplicationBuilder) extends ApplicationLoader {
   def this() = this(new ScaldiApplicationBuilder())
 
-  def load(context: Context)  =
+  def load(context: Context): Application =
     builder
       .in(context.environment)
       .loadConfig(context.initialConfiguration)
       .prependModule(new Module {
-        bind [OptionalSourceMapper] to new OptionalSourceMapper(context.sourceMapper)
-        bind [WebCommands] to context.webCommands
+        bind [OptionalDevContext] to new OptionalDevContext(context.devContext)
+        // TODO: evaluate if `OptionalSourceMapper` still needs to be bound
+        bind [OptionalSourceMapper] to new OptionalSourceMapper(context.devContext.map(_.sourceMapper))
       })
-      .build
+      .build()
 }
